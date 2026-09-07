0d415d127ba445faa593a14e8c01239e05ece87e
Zaelani <bahar32harz@gmail.com>
2026-09-07 22:06:20 +0700

    update footer and navbar design, and refactor route web .

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -2,13 +2,13 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top shadow-sm">
     <div class="container">
         <a class="navbar-brand fw-bold text-primary fs-4" href="{{ url('/') }}">
-            <i class="bi bi-shop me-2"></i>UMKM Store
+            <i class="bi bi-shop me-2"></i>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto">
+            <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
                     <a class="nav-link {{ request()->is('/') ? 'active' : '' }}" href="{{ url('/') }}">Beranda</a>
                 </li>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::name("public.")->group(function() {
+    Route::get('/', function () {
+        return view('welcome');
+    })->name("home");
 });
